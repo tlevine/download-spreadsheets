827c0985fc18a6e6b27d0fc8b406cef702569b1a
Thomas Levine <_@thomaslevine.com>
2014-03-11 17:02:34 +0000

    expectation

--- featured_spreadsheets/test/test_examine.py
+++ featured_spreadsheets/test/test_examine.py
@@ -7,5 +7,9 @@ from featured_spreadsheets.examine import unique_indices
 def test_unique_indices():
     fp = StringIO('a;b;c;d\r\n3;4;5;6')
     observed = unique_indices(fp, 'url is just for logging')
-    expected = {('a','b','c'),('b','c','d')} # only adjacent, max 3
+    expected = {
+        ('a',), ('b',), ('c',),
+        ('a','b'), ('b','c'),('c','d'),
+        ('a','b','c'), ('b','c','d'),
+    } # only adjacent, max 3
     n.assert_set_equal(observed, expected)
